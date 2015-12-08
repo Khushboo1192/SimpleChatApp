5995bc762a2a6b941388514630ba1cdd8de299f0
Khushboo Mandlecha <khushboomandlecha@khushboos-macbook-pro.local>
2015-12-07 23:54:15 -0500

    Initial Commit

---
+++ SimpleChatApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimpleChatApp
+//
+//  Created by Khushboo Mandlecha on 12/7/15.
+//  Copyright © 2015 Khushboo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
